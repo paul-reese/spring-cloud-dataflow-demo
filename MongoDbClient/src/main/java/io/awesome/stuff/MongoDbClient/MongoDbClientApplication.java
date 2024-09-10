f092ce6273703d5bc72a87573abfbab3c1a0c337
paul-reese <paul.reese@gmail.com>
2024-09-10 16:08:07 -0600

    adding Spring starter to connect to MongoDB Atlas

---
+++ MongoDbClient/src/main/java/io/awesome/stuff/MongoDbClient/MongoDbClientApplication.java
@@ -0,0 +1,13 @@
+package io.awesome.stuff.MongoDbClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbClientApplication.class, args);
+	}
+
+}
